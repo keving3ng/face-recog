3f783e310e1f6f3404f6a46b16961c7c49cb4456
Kevin Geng <kevin.geng@quarksys.ca>
2019-12-08 22:26:29 -0500

    added meaure face function

--- detect_glasses.py
+++ detect_glasses.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-# face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('cascades/haarcascade_eye_tree_eyeglasses.xml')
 
 def focusEye(img, eyeLoc):
@@ -9,17 +9,23 @@ def focusEye(img, eyeLoc):
     eyeCropped = img[y:y+h, x:x+w]
     cv2.imshow('Eye Cropped', eyeCropped)
 
-def detectFace(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    eyes = eye_cascade.detectMultiScale(gray, 1.3, 5)
+def detectEyes(img):
+    eyes = eye_cascade.detectMultiScale(img, 1.3, 5)
     for (x,y,w,h) in eyes:
         img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
 
-    focusEye(img, eyes[0])
+    if len(eyes) > 0:
+        focusEye(img, eyes[0])
 
 def captureAndDetect(img):
-    detectFace(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    w, h = measureFace(gray)
+    detectEyes(gray)
     cv2.imshow('Eye Detection', img)
+
+def measureFace(img):
+    x, y, w, h = face_cascade.detectMultiScale(img, 1.3, 5)
+    return w, h
 
 def show_webcam():
     cam = cv2.VideoCapture(0)
@@ -28,6 +34,7 @@ def show_webcam():
         img = cv2.flip(img, 1)
         cv2.imshow('Webcam Main', img)
         k = cv2.waitKey(1)
+
         if k == 32: # Space to Capture and Detect
             captureAndDetect(img)
         elif k == 27: # ESC key to exit
