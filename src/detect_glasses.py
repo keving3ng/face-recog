bf7eb882fadca6b6ace16af6edcf2c7f5916bd1b
Kevin Geng <kevin.geng@quarksys.ca>
2019-12-09 20:05:35 -0500

    changed to class, split height padding

--- src/detect_glasses.py
+++ src/detect_glasses.py
@@ -4,99 +4,103 @@ import cv2
 face_cascade = cv2.CascadeClassifier('data/cascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('data/cascades/haarcascade_eye_tree_eyeglasses.xml')
 
-def focusEye(img, eyeLoc, pad):
-    '''
-        Crops location of eye with padding to capture area with eye glasses
+class DetectGlasses:
+    def __init__(self):
+        self.show_webcam()
 
-        Args:
-            img                   (img): image of face
-            eyeLoc (int, int, int, int): x, y coords of eye, width, height of eye
-            pad              (int, int): padding around eye to capture glasses
-        Returns:  None
-        Raises:   None
-    '''
-    x, y, w, h = eyeLoc
-    wPad, hPad = pad
+    def focusEye(self, img, eyeLoc, pad):
+        '''
+            Crops location of eye with padding to capture area with eye glasses
 
-    eyeCropped = img[y-hPad:y+h+hPad, x-wPad:x+w+wPad]
-    cv2.imshow('Eye Cropped', eyeCropped)
+            Args:
+                img                   (img): image of face
+                eyeLoc (int, int, int, int): x, y coords of eye, width, height of eye
+                pad              (int, int): padding around eye to capture glasses
+            Returns:  None
+            Raises:   None
+        '''
+        x, y, w, h = eyeLoc
+        wPad, hPadBot, hPadTop = pad
 
-def detectEyes(face, pad):
-    '''
-        Detects eyes on image of face, then draws a rectangle on the first one detected.
-        Then, calls another function to crop out the area around the eye.
+        eyeCropped = img[y-hPadTop:y+h+hPadBot, x-wPad:x+w+wPad]
+        cv2.imshow('Eye Cropped', eyeCropped)
 
-        Args:
-            face     (img): image of the face
-            pad (int, int): padding of eye area
-        Returns:  None
-        Raises:   None
-    '''
-    eyes = eye_cascade.detectMultiScale(face, 1.3, 5)
-    if len(eyes) > 0:
-        x, y, w, h = eyes[0]
-        face = cv2.rectangle(face,(x,y),(x+w,y+h),(0,0,255),2)
+    def detectEyes(self, face, pad):
+        '''
+            Detects eyes on image of face, then draws a rectangle on the first one detected.
+            Then, calls another function to crop out the area around the eye.
 
-        focusEye(face, eyes[0], pad)
+            Args:
+                face     (img): image of the face
+                pad (int, int): padding of eye area
+            Returns:  None
+            Raises:   None
+        '''
+        eyes = eye_cascade.detectMultiScale(face, 1.3, 5)
+        if len(eyes) > 0:
+            x, y, w, h = eyes[0]
+            face = cv2.rectangle(face,(x,y),(x+w,y+h),(0,0,255),2)
 
-def calcPadding(w, h, wPct=0.5, hPct=0.33):
-    '''
-        Calculates padding around eye region based on face area size to reduce search area for eyeglasses.
+            self.focusEye(face, eyes[0], pad)
 
-        Args:
-            w      (int): width of face
-            h      (int): height of face
-            wPct (float): Ratio to scale the width padding. Approximate percentage of glasses coverage of face of ONE eye
-            hPct (float): See above, but for height on face.
-        Returns:
-            'face width padding', 'face height padding'
-        Raises:  None
-    '''
-    return int(w * wPct / 4), int(h * hPct / 4)
+    def calcPadding(self, w, h, wPct=0.5, hPct=0.33, split=0.85):
+        '''
+            Calculates padding around eye region based on face area size to reduce search area for eyeglasses.
+            Since glasses usually will typically down below the eye instead of above, the height padding is split
+            unevenly over the top and bottom
 
-def detectFace(img):
-    '''
-        Detects face(s) using haar cascade from grayscale image. When face is found, goes to detect eyes
-        on image of face area (reducing search area).
+            Args:
+                w       (int): width of face
+                h       (int): height of face
+                wPct  (float): ratio to scale the width padding. Approximate percentage of glasses coverage of face of ONE eye
+                hPct  (float): see above, but for height on face.
+                split (float): split of top/bottom padding of eye region
+            Returns:
+                'face width padding', 'face height top padding', 'face height bottom padding'
+            Raises:  None
+        '''
+        return int(w * wPct / 4), int(h * hPct / 4 * split), int(h * hPct / 4 * (1 - split))
 
-        Args:
-            img (img): image from webcam input to process
-        Returns:  None
-        Raises:   None
-    '''
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    # For now, this is designed to work with one face
-    if len(faces) > 0:
-        x, y, w, h = faces[0]
-        face = img[y:y+h, x:x+w]
-        detectEyes(face, calcPadding(w, h))
+    def detectFace(self, img):
+        '''
+            Detects face(s) using haar cascade from grayscale image. When face is found, goes to detect eyes
+            on image of face area (reducing search area).
 
-def show_webcam():
-    '''
-        This function loops to call functions to show webcam video and process the images.
+            Args:
+                img (img): image from webcam input to process
+            Returns:  None
+            Raises:   None
+        '''
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        # For now, this is designed to work with one face
+        if len(faces) > 0:
+            x, y, w, h = faces[0]
+            face = img[y:y+h, x:x+w]
+            self.detectEyes(face, self.calcPadding(w, h))
 
-        Args:     None
-        Returns:  None
-        Raises:   None
-    '''
-    cam = cv2.VideoCapture(0)
+    def show_webcam(self, ):
+        '''
+            This function loops to call functions to show webcam video and process the images.
 
-    while True:
-        ret_val, img = cam.read()
-        img = cv2.flip(img, 1)
-        detectFace(img)
+            Args:     None
+            Returns:  None
+            Raises:   None
+        '''
+        cam = cv2.VideoCapture(0)
 
-        cv2.imshow('Webcam Main', img)
+        while True:
+            ret_val, img = cam.read()
+            img = cv2.flip(img, 1)
+            self.detectFace(img)
 
-        k = cv2.waitKey(1)
-        if k == 27: # ESC key to exit
-            break
+            cv2.imshow('Webcam Main', img)
 
-    cv2.destroyAllWindows()
+            k = cv2.waitKey(1)
+            if k == 27: # ESC key to exit
+                break
 
-def main():
-    show_webcam()
+        cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main()
+    DetectGlasses()
