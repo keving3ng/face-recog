ce961e38185663302b61e36d6dcf7e313200a818
Kevin Geng <kevin.geng@quarksys.ca>
2020-01-03 12:17:02 -0500

    added imagedatagenerator and some model stuff

--- src/classify_eyes.py
+++ src/classify_eyes.py
@@ -9,15 +9,26 @@ PATH = "./data/glasses"
 train_dir = os.path.join(PATH, "train")
 validation_dir = os.path.join(PATH, "validation")
 
-train_glasses_dir = os.path.join(train_dir, "glasses")
-train_regular_dir = os.path.join(train_dir, "regular")
-validation_glasses_dir = os.path.join(validation_dir, "glasses")
-validation_regular_dir = os.path.join(validation_dir, "regular")
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# train_glasses_dir = os.path.join(train_dir, "glasses")
+# train_regular_dir = os.path.join(train_dir, "regular")
+# validation_glasses_dir = os.path.join(validation_dir, "glasses")
+# validation_regular_dir = os.path.join(validation_dir, "regular")
 
 def trainModel():
+	# Rescale all images by 1./255
+	train_datagen = ImageDataGenerator(rescale=1/255)
+
+	train_generator = train_datagen.flow_from_directory(
+		train_dir,
+		target_size=(128, 128),
+		batch_size=1,
+		class_mode='binary'
+	)
 
 	model = tf.keras.models.Sequential([
-		tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(256, 256, 3)),
+		tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(128, 128, 3)),
 		tf.keras.layers.MaxPooling2D(2, 2),
 		tf.keras.layers.Flatten(), 
 		tf.keras.layers.Dense(128, activation='relu'),
@@ -30,7 +41,12 @@ def trainModel():
 				  optimizer=RMSprop(lr=0.001),
 				  metrics=['acc'])
 
-	
+	history = model.fit_generator(
+		train_generator,
+		steps_per_epoch=8, 
+		epochs=10,
+		verbose=1
+	)
 
 if __name__ == "__main__":
 	print("Tensorflow version= " + tf.__version__)
