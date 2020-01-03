03212e3ea42f6fb54c56c76b75117400a5571610
Kevin Geng <kevin.geng@quarksys.ca>
2020-01-03 11:56:39 -0500

    set up basic model stuff

--- src/classify_eyes.py
+++ src/classify_eyes.py
@@ -1,12 +1,8 @@
 import os
-
 import numpy as np
-
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.layers import (Conv2D, Dense, Dropout, Flatten,
-                                     MaxPooling2D)
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 PATH = "./data/glasses"
@@ -18,8 +14,24 @@ train_regular_dir = os.path.join(train_dir, "regular")
 validation_glasses_dir = os.path.join(validation_dir, "glasses")
 validation_regular_dir = os.path.join(validation_dir, "regular")
 
+def trainModel():
 
-print(len(os.listdir(train_glasses_dir)))
-print(len(os.listdir(train_regular_dir)))
-print(len(os.listdir(validation_glasses_dir)))
-print(len(os.listdir(validation_regular_dir)))
+	model = tf.keras.models.Sequential([
+		tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(256, 256, 3)),
+		tf.keras.layers.MaxPooling2D(2, 2),
+		tf.keras.layers.Flatten(), 
+		tf.keras.layers.Dense(128, activation='relu'),
+		tf.keras.layers.Dense(1, activation='sigmoid')
+	])
+
+	model.summary()
+
+	model.compile(loss='binary_crossentropy',
+				  optimizer=RMSprop(lr=0.001),
+				  metrics=['acc'])
+
+	
+
+if __name__ == "__main__":
+	print("Tensorflow version= " + tf.__version__)
+	trainModel()
